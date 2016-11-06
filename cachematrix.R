e7a7b78ff88111b37ddf2413d84ffcea7cf1dce6
UrbanVillager <nitishjaat33@gmail.com>
2016-11-06 22:12:18 +0530

    My ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Basic function of this program is to reduce the computation time
+## of those calculations where same data is processed many times.
 
-## Write a short comment describing this function
+## In makeCacheMatrix function, I have fixed the environment for 
+## input matrix(x) and its inverse(i).
 
-makeCacheMatrix <- function(x = matrix()) {
+## Initially inverse matrix(i) is set to NULL so as to hold only
+## place without any value.
 
+## In the end all functions are included in a list and given names
+## so that they can be accessed from outside of makeCacheMatrix function.
+
+makeCacheMatrix <- function(x = matrix())
+{
+  i <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) i <<- solve
+  getInverse <- function() i
+  list(set = set, get = get, setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Main function of cacheSolve is to first check the value of
+## inverse in Cache and retreive it if it is not NULL.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## In case value of Inverse Matrix(i) is NULL then it computes
+## it and set its value in the Cache
+
+## In the end it returns the inverse of input matrix
+
+cacheSolve <- function(x, ...)
+{
+  i <- x$getInverse()
+  if(!is.null(i))
+  {
+    message('Retreiving cache data')
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data,...)
+  x$setInverse(i)
+  i
 }
